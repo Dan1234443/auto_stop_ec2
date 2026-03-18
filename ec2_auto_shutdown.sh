57053e652a7c17fe2045e9c475aebd33569a18ee
Ubuntu <ubuntu@ip-172-31-16-170.ec2.internal>
2026-03-18 04:15:31 +0000

    slighly changed script syntax

--- ec2_auto_shutdown.sh
+++ ec2_auto_shutdown.sh
@@ -1,26 +1,15 @@
 #!/bin/bash
 
-# Author: Dan-Herete Agha
-# Project: EC2 Auto Shutdown Script
-# Description: Automatically stops EC2 instance when SSH sessions ends
+# Wait for logout to complete
+sleep 10
 
-# Count active SSH sessions
-ACTIVE_USERS=$(who | grep pts | wc -l)
+# Count sessions
+ACTIVE_USERS=$(pgrep -u ubuntu sshd | wc -l)
 
 echo "Active SSH sessions: $ACTIVE_USERS"
 
-# Get IMDSv2 token
-TOKEN=$(curl -X PUT -s "http://169.254.169.254/latest/api/token" \
--H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
-
-# Get EC2 instance ID
-INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
-http://169.254.169.254/latest/meta-data/instance-id)
-
-REGION="us-east-1"
-
-# If no active SSH sessions remain
 if [ "$ACTIVE_USERS" -eq 0 ]; then
+
     echo "No active SSH sessions detected."
 
     # Safety delay
@@ -34,6 +23,7 @@ if [ "$ACTIVE_USERS" -eq 0 ]; then
     else
         echo "User logged in again. Shutdown cancelled."
     fi
+
 else
     echo "Users still logged in. Instance stays running."
 fi
