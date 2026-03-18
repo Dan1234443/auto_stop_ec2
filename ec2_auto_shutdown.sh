91cae2f6566758146f39639378f7cb8a0763f5c0
Ubuntu <ubuntu@ip-172-31-16-170.ec2.internal>
2026-03-18 21:05:14 +0000

    Changed entire script format

--- ec2_auto_shutdown.sh
+++ ec2_auto_shutdown.sh
@@ -1,29 +1,24 @@
 #!/bin/bash
 
-# Wait for logout to complete
-sleep 10
+echo "Script started at $(date)" >> /home/ubuntu/script/debug.log
 
-# Count sessions
-ACTIVE_USERS=$(pgrep -u ubuntu sshd | wc -l)
+sleep 15
 
-echo "Active SSH sessions: $ACTIVE_USERS"
+REGION="us-east-1"
+
+TOKEN=$(curl -X PUT -s "http://169.254.169.254/latest/api/token" \
+-H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" \
+http://169.254.169.254/latest/meta-data/instance-id)
+
+ACTIVE_USERS=$(who | wc -l)
+
+echo "Active SSH sessions: $ACTIVE_USERS" >> /home/ubuntu/script/debug.log
 
 if [ "$ACTIVE_USERS" -eq 0 ]; then
-
-    echo "No active SSH sessions detected."
-
-    # Safety delay
-    sleep 60
-
-    ACTIVE_USERS=$(who | grep pts | wc -l)
-
-    if [ "$ACTIVE_USERS" -eq 0 ]; then
-        echo "Stopping EC2 instance..."
-        aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$REGION"
-    else
-        echo "User logged in again. Shutdown cancelled."
-    fi
-
+    echo "Stopping instance..." >> /home/ubuntu/script/debug.log
+    aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$REGION"
 else
-    echo "Users still logged in. Instance stays running."
+    echo "Still active users" >> /home/ubuntu/script/debug.log
 fi
