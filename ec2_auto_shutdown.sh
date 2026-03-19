e8ed7ac3d433a557d1e8834e6fedbb51a137531b
Ubuntu <ubuntu@ip-172-31-16-170.ec2.internal>
2026-03-19 02:19:22 +0000

    changed script syntax

--- ec2_auto_shutdown.sh
+++ ec2_auto_shutdown.sh
@@ -16,9 +16,24 @@ ACTIVE_USERS=$(who | wc -l)
 
 echo "Active SSH sessions: $ACTIVE_USERS" >> /home/ubuntu/script/debug.log
 
-if [ "$ACTIVE_USERS" -eq 0 ]; then
-    echo "Stopping instance..." >> /home/ubuntu/script/debug.log
-    aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$REGION"
+if [ "$ACTIVE_USERS" -le 1 ]; then
+
+    echo "Potential logout detected. Waiting..." >> /home/ubuntu/script/debug.log
+
+    sleep 60
+
+    ACTIVE_USERS=$(who | wc -l)
+
+    if [ "$ACTIVE_USERS" -le 1 ]; then
+        echo "Stopping instance..." >> /home/ubuntu/script/debug.log
+
+        aws ec2 stop-instances --instance-ids "$INSTANCE_ID" --region "$REGION" >> /home/ubuntu/script/debug.log 2>&1
+
+        echo "Shutdown command executed." >> /home/ubuntu/script/debug.log
+    else
+        echo "User reconnected. Shutdown cancelled." >> /home/ubuntu/script/debug.log
+    fi
+
 else
     echo "Still active users" >> /home/ubuntu/script/debug.log
 fi
